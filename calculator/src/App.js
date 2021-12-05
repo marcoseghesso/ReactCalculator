7a1cd2c944b93feadbce3541219661f22b5a7c6e
Marco Seghesso <mseghesso@gmail.com>
2021-12-06 11:39:22 +1300

    Dark Mode completed

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,6 +2,7 @@ import { useReducer } from "react";
 import "./styles.css"
 import DigitButton from "./DigitButton";
 import OperationButton from "./OperationButton";
+import DarkMode from "./Darkmode";
 
 export const ACTIONS = {
   CHOOSE_OPERATION: "choose-operation",
@@ -134,30 +135,38 @@ function App() {
   const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,{})
 
   return (
-    <div className="calculator-grid">
-      <div className="output">
-        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
-        <div className="current-operand">{formatOperand(currentOperand)}</div>
+    <div>
+      <div>
+        <nav>
+          <h4>Dual Theme Calculator</h4>
+          <DarkMode />
+        </nav>
       </div>
-      <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR })}>AC</button>
-      <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT })}>DEL</button>
-      <OperationButton operation="/" dispatch={dispatch} />
-      <DigitButton digit="1" dispatch={dispatch} />
-      <DigitButton digit="2" dispatch={dispatch} />
-      <DigitButton digit="3" dispatch={dispatch} />
-      <OperationButton operation="x" dispatch={dispatch} />
-      <DigitButton digit="4" dispatch={dispatch} />
-      <DigitButton digit="5" dispatch={dispatch} />
-      <DigitButton digit="6" dispatch={dispatch} />
-      <OperationButton operation="+" dispatch={dispatch} />
-      <DigitButton digit="7" dispatch={dispatch} />
-      <DigitButton digit="8" dispatch={dispatch} />
-      <DigitButton digit="9" dispatch={dispatch} />
-      <OperationButton operation="-" dispatch={dispatch} />
-      <DigitButton digit="." dispatch={dispatch} />
-      <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE })}>=</button>
-    </div>
+      <div className="calculator-grid">
+        <div className="output">
+          <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+          <div className="current-operand">{formatOperand(currentOperand)}</div>
+        </div>
+        <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR })}>AC</button>
+        <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT })}>DEL</button>
+        <OperationButton operation="/" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
+        <OperationButton operation="x" dispatch={dispatch} />
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
+        <button className="span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE })}>=</button>
+      </div>
+    </div>    
   );
 }
 
